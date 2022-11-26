5fb33cd00b3c03416c622105d013000a5e563eab
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2022-11-27 02:24:36 +1100

    refactor: add css file for basic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import City from "./components/City"
-import OtherCities from "./components/OtherCities"
-import Forecast from "./components/Forecast"
+import City from './components/City';
+import OtherCities from './components/OtherCities';
+import Forecast from './components/Forecast';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <City />
-      <div>
-        <OtherCities />
-        <Forecast />
+    <div className="container">
+      <div className="card">
+        <City />
+        <div className="bottom">
+          <OtherCities />
+          <Forecast />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
