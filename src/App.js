e6a91cccf1c8909e5c0d99457dd215c93f1363f2
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2022-11-28 01:21:49 +1100

    refactor: add styled component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import City from './components/City';
 import OtherCities from './components/OtherCities';
 import Forecast from './components/Forecast';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url(https://wallpaperaccess.com/full/2629319.png);
+  background-size: cover;
+`;
+
+const Card = styled.div`
+  width: 800px;
+  height: 600px;
+  background-color: white;
+  border-radius: 32px;
+  box-shadow: 0 0 16px rgb(0 0 0 / 50%);
+`;
+
+const Bottom = styled.div`
+  display: flex;
+`;
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="card">
+    <Container>
+      <Card>
         <City />
-        <div className="bottom">
+        <Bottom>
           <OtherCities />
           <Forecast />
-        </div>
-      </div>
-    </div>
+        </Bottom>
+      </Card>
+    </Container>
   );
 };
 
